31e65f177a865f2a2ff0fce6a0c02474c8a495bf
Alex T <lexkie@proton.me>
2025-12-01 00:38:11 -0500

    Add files via upload

---
+++ Pluvia.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#author: alex terranova
+#date:11/08/2025
+#name:Pluvia
+#version:1.0final
+#description: Brainwave-Entrainment/ASMR
+
+ffplay  -loop 0 -nodisp -autoexit "./Pluvia/Rain_light_consistent_leaves.flac" > /dev/null 2>&1 &
+ffplay  -loop 0 -nodisp -autoexit "./Pluvia/Rain_mod_light_drippy_aftermath.flac" > /dev/null 2>&1 &
+ffplay  -loop 0 -nodisp -autoexit "./Pluvia/Rain_light_leaf_drips.flac" > /dev/null 2>&1 &
+
+wait
+
+
+
+echo "Now playing: Pluvia..."
+
+wait
